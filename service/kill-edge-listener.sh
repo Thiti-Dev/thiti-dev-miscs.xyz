3da9358e92fdaf2cc1d3800c6603fff11ded679f
Thiti-Dev <thiti.mwk.main@gmail.com>
2023-01-07 15:09:42 +0700

    made kill-edge-listener extract the EDGE_RUNTIME_PORT from the .env

--- service/kill-edge-listener.sh
+++ service/kill-edge-listener.sh
@@ -1,3 +1,4 @@
-pid=$(lsof -t -i:3000)
+edge_runtime_port=$(grep -v '^#' .env | grep -e "EDGE_RUNTIME_PORT" | sed -e 's/.*=//') # extract the env variable
+pid=$(lsof -t -i:$edge_runtime_port)
 kill $pid 2> /dev/null #silence the error output <1 for regular info>
 exit 0
