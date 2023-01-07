98202901a4fc9460948cd218ee6078fba1984b9b
Thiti-Dev <thiti.mwk.main@gmail.com>
2023-01-07 15:10:41 +0700

    implemented edge-runtime-local-simulated-entry.js with custom environment injection

---
+++ service/edge-runtime-local-simulated-entry.js
@@ -0,0 +1,31 @@
+import { runServer, EdgeRuntime } from "edge-runtime";
+import fs from "fs";
+import path from "path";
+
+const __dirname = path.resolve(path.dirname(""));
+
+const runtimeScriptAsTxt = fs.readFileSync(
+  path.resolve(__dirname, "./dist/index.js"),
+  "utf8"
+);
+
+const runtimeScriptAsTxtWithProcessEnvSimulated = runtimeScriptAsTxt.replace(
+  /"use strict";/,
+  `
+
+var process = {
+  env: ${JSON.stringify(process.env)}, // populate all the env
+};
+
+`
+);
+
+const runtime = new EdgeRuntime({
+  initialCode: runtimeScriptAsTxtWithProcessEnvSimulated,
+});
+const server = await runServer({
+  runtime,
+  port: process.env.EDGE_RUNTIME_PORT || 3000,
+});
+
+console.log(`Listening at ${server.url}`);
