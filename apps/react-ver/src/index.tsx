897da355791400ce2e40ffdb550fd1e52fa86b5a
Thiti-Dev <thiti.mwk.main@gmail.com>
2023-01-08 22:31:27 +0700

    init react-router-dom

--- apps/react-ver/src/index.tsx
+++ apps/react-ver/src/index.tsx
@@ -1,16 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LandingPage from "./views/landing-page";
+import Registration from "./views/registration";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LandingPage />,
+    loader: undefined,
+    children: [],
+  },
+  { path: "/register", element: <Registration />, loader: undefined },
+]);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <RouterProvider router={router} />
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ apps/react-ver/src/views/registration/registration.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Registration: React.FC = () => {
+  return <div>Registration Page</div>;
+};
+
+export default Registration;
