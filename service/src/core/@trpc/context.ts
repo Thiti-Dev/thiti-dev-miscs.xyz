743fdb43c77114004a7f80c5748000cf63c7c336
Thiti-Dev <thiti.mwk.main@gmail.com>
2023-01-08 15:32:49 +0700

    @trpc: implemented (fac-fn)getCreateContextHandler that pass along the derived env from caller

--- service/src/core/@trpc/context.ts
+++ service/src/core/@trpc/context.ts
@@ -1,7 +1,25 @@
+import { PrismaClient } from "@prisma/client/edge";
 import { inferAsyncReturnType } from "@trpc/server";
 import { FetchCreateContextFnOptions } from "@trpc/server/adapters/fetch";
-export function createContext({ req }: FetchCreateContextFnOptions) {
+import { Env } from "../..";
+import PrismaInstance from "../@prisma";
+export function createContext({ req }: FetchCreateContextFnOptions, env: Env) {
+  const prismaInstance = new PrismaInstance(
+    new PrismaClient({
+      datasources: {
+        db: {
+          url: env.DB_URL,
+        },
+      },
+    })
+  );
+
   const user = { name: req.headers.get("username") ?? "anonymous" };
-  return { req, user };
+  return { req, user, prisma: prismaInstance };
 }
+
+export function getCreateContextHandler(req: Request, env: Env) {
+  return createContext.bind(null, { req }, env);
+}
+
 export type Context = inferAsyncReturnType<typeof createContext>;
