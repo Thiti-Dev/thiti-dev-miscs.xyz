1e841b3c084b801963cec8c2e3e9d8aca46afc13
Thiti-Dev <thiti.mwk.main@gmail.com>
2023-01-08 15:33:44 +0700

    transformed the entry to work with cfwk

--- service/src/index.ts
+++ service/src/index.ts
@@ -1,14 +1,19 @@
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
-import { createContext } from "./core/@trpc/context";
+import { getCreateContextHandler } from "./core/@trpc/context";
 import { appRouter } from "./core/@trpc/router";
 
-addEventListener("fetch", (event) => {
-  return event.respondWith(
-    fetchRequestHandler({
-      endpoint: "/trpc",
-      req: event.request,
-      router: appRouter,
-      createContext,
-    })
-  );
-});
+export interface Env {
+  DB_URL: string;
+}
+async function fetch(request: Request, env: Env): Promise<Response> {
+  return fetchRequestHandler({
+    endpoint: "/trpc",
+    req: request,
+    router: appRouter,
+    createContext: getCreateContextHandler(request, env),
+  });
+}
+
+export default {
+  fetch,
+};
